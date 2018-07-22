5b44170e87d6afd9979a937674b2339eb883e93c
Edward Castillo <edwardcastillo@me.com>
2018-07-22 00:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function dwarfRollCall(dwarves) {
-
+  for (i = 0; i < dwarves.length; i++) {
+    dwarves.push(`${dwarves[i]}. ${dwarves[i]}`);
+  } return(dwarves);
 }
 
 function summonCaptainPlanet(planeteerCalls){
